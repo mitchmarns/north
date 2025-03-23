471474c52dca85cc54837a1b25a5f3eabdff664e
mitchmarns <sakurapizzaa@gmail.com>
2025-03-23 09:04:30 -0700

    import

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -1,4 +1,4 @@
-const { Character, User, Relationship, Team, sequelize, Sequelize } = require('../models');
+const { Character, User, Relationship, Team, CharacterGallery, sequelize, Sequelize } = require('../models');
 const { validationResult } = require('express-validator');
 
 // Get all characters (public)
