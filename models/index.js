f0ce4524094ec4d18e6897b076a59ef06bdd3509
mitchmarns <sakurapizzaa@gmail.com>
2025-03-24 14:05:26 -0700

    fix

--- models/index.js
+++ models/index.js
@@ -30,8 +30,6 @@ const Message = require('./messages')(sequelize, Sequelize.DataTypes);
 const SocialPost = require('./socialPost')(sequelize, Sequelize.DataTypes);
 const Comment = require('./comment')(sequelize, Sequelize.DataTypes);
 const Like = require('./like')(sequelize, Sequelize.DataTypes);
-const GroupConversation = require('./groupConversation')(sequelize, Sequelize.DataTypes);
-const GroupMember = require('./groupMember')(sequelize, Sequelize.DataTypes);
 
 // Define model associations
 User.hasMany(Character, { foreignKey: 'userId', as: 'characters' });
